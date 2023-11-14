7256c355a600dd0975a4a79c00bc35a7cad6d04f
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-15 01:02:04 +0900

    test: 예상 방문 날짜 1~31이하 숫자가 아닌 경우 테스트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import InputView from "./views/InputView.js";
 import OutputView from "./views/OutputView.js";
 import EventBenefit from "./domains/EventBenefit.js";
 
-
 class App {
   async run() {
     OutputView.printWelcomeMessage();
