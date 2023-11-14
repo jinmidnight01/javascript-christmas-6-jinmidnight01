eebc4ac9f9df9b07d331aac3460ce6034794f6e2
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-14 15:59:51 +0900

    refactor(App): 입력창 재귀함수(recursiveInput) 구현
    - readDate 함수 분리

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import InputView from "./views/InputView.js";
 import OutputView from "./views/OutputView.js";
 import EventBenefit from "./domains/EventBenefit.js";
 
+
 class App {
   async run() {
     OutputView.printWelcomeMessage();
-    const date = await InputView.readDate();
-    const orders = await InputView.readOrders();
-    const eventBenefit = new EventBenefit(date, orders);
+    const date = await this.recursiveInput(this.readDate, this)
     OutputView.printBenefitsPreviewTitle();
   }
+
+  async readDate() {
+    const date = await InputView.readDate();
+    EventBenefit.validateDate(date);
+    return date;
+  }
+
+  async recursiveInput(fn, context) {
+    try {
+      return await fn.call(context);
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.recursiveInput(fn, context);
+    }
+  }
+
 }
 
 export default App;
