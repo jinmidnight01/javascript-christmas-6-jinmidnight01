666a49ee40902b1749177cd45002174e7c228469
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-15 17:03:01 +0900

    test: 메뉴 개수가 1이상의 자연수가 아닌 경우

--- __tests__/ValidateDateTest.js
+++ __tests__/ValidateDateTest.js
@@ -1,9 +1,9 @@
-import EventBenefit from "../src/domains/EventBenefit.js";
+import Validations from "../src/domains/Validations";
 
 describe("예상 방문 날짜", () => {
   test.each([[
     ["0", "21.1", "300", "-12", "sdf"]
   ]])("1이상 31이하 숫자가 아닌 경우", (dateString) => {
-    expect(() => EventBenefit.validateDate(dateString)).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    expect(() => Validations.isInRange(dateString)).toThrow("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
   });
 });
